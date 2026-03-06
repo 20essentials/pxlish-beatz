8321ab08fae02bb75ed2985b96c17c86962ca3d0
DominicDev <s0l0estudi4r@gmail.com>
2026-03-06 16:59:11 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PXLISH BEATZ';
+export const nameSongs = [
+	"DAYDREAM", /*1*/
+	"PARTY PARTY PHONK", /*2*/
+	"SPIT IN MY FACE!", /*3*/
+	"ALL THE THINGS SHE SAID", /*4*/
+	"MISS YOU", /*5*/
+	"DIMENSIONS", /*6*/
+	"PHONKSTAR ", /*7*/
+	"SMURF CAT", /*8*/
+	"LOST CASSETTE", /*9*/
+	"DIED IN YOUR ARMS", /*10*/
+	"REVENGE ", /*11*/
+	"123 MOVE!", /*12*/
+	"ALORS ON DANSE", /*13*/
+	"VINGANÇA E MATAR", /*14*/
+	"CATASTROPHE", /*15*/
+	"AROUND BRAZIL", /*16*/
+	"HOT N COLD", /*17*/
+	"WE ARE THE PEOPLE", /*18*/
+	"KINGDOM", /*19*/
+	"BALALAIKA PHONK", /*20*/
+]
